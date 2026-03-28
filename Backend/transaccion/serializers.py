459fa0e14b2e3c89620e93577511ff17664b0e94
Mayker09 <160505094+Mayker09@users.noreply.github.com>
2026-03-28 15:06:51 -0400

    eliminando archivos y configuraciones de las aplicaciones categoria, presupuesto y transaccion

--- Backend/transaccion/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import transaccion
-
-class transaccionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = transaccion
-        fields = '__all__'
